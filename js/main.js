68505051beafc612d6953871fa6e82da5f0853d5
Ahmed Moneim <ahnedmoneimhacher26@gmail.com>
2025-08-25 13:27:30 +0300

    تعديلات على index.html و main.css و main.js

--- js/main.js
+++ js/main.js
@@ -151,6 +151,180 @@ let Cancel = document.querySelector(".cancel") ;
 Cancel.addEventListener("click", function () {
   form1.style.display = "block";
   form2.style.display = "none";
+  document.querySelector("#signupf").reset();
+
 });
+
+
+// ================ users database ================= 
+
+
+// دالة لإضافة مستخدم وحفظة في الذاكرة الدائمة
+
+function add_save(new_user) {
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  users.push(new_user);
+  localStorage.setItem("users", JSON.stringify(users));
+  alert("تم إنشاء حساب جديد بنجاح ");
+}
+// دالة الحصول على البيانات من المستخدم 
+function getData() {
+  return {
+    fname: document.querySelector("#firstName").value,
+    lname: document.querySelector("#lastName").value,
+    email: document.querySelector("#signemail").value,
+    passw: document.querySelector("#signpass").value,
+    confpassw: document.querySelector("#confirpass").value
+  };
+}
+// دالة التأكد من عدم وجود حقول فارغة 
+function inputValidation(form) {
+  let inputs = form.querySelectorAll("input");     
+  let allFilled = true; 
+
+  inputs.forEach(input => {
+    if (input.value.trim() === "") {               
+      allFilled = false;
+      input.style.border = "2px solid red";        
+    } else {
+      input.style.border = "";                     
+    }
+  });
+
+  return allFilled; 
+}
+// دالة إنشاء الحساب الجديد
+function signIn() {
+  let User = getData();
+  let confirmPass = document.querySelector("#confirpass");
+
+  if (User.passw !== User.confpassw) {
+    confirmPass.setCustomValidity("كلمة المرور غير متطابقة");
+    confirmPass.reportValidity(); 
+    return;
+  } else {
+    confirmPass.setCustomValidity(""); 
+  }
+  add_save(User);
+  form2.style.display = "none";
+  let user_board = document.querySelector(".container2");
+  user_board.style.display = "block";
+  document.querySelector("#signupf").reset();
+
+  // إضافة بيانات التسجيل للبروفايل
+  setProfileinfo(User)
+  
+}
+
+
+// الزرار الخاص بإنشاء الحساب
+let createBtn = document.querySelector("#create-acount");
+let logBtn = document.querySelector("#login-button");
+ 
+// دالة التأكد من وجود بريد مسبق 
+  function emailExists(email) {
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  return users.some(user => user.email === email);
+}
+// دالة تسجيل الدخول 
+function Login(){
+  let log_email = document.querySelector("#inputEmail3").value ;
+  let log_password = document.querySelector("#inputPassword3").value ;
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  let foundUser = users.find(user => user.email === log_email && user.passw === log_password);
+  if(foundUser)
+  {
+    alert("تم تسجيل الدخول بنجاح ") ;
+    setProfileinfo(foundUser) ;
+
+  }
+  else 
+  {
+    alert("البريد أو كلمة المرور غير صحيحة !") ;
+  }
+}
+// إجراء تسجيل الدخول 
+logBtn.addEventListener("click" , function(e){
+  let form = e.target.closest("form"); // الفورم اللي الزرار موجود فيه
+  if (!inputValidation(form)) {
+    alert("⚠️ من فضلك املأ جميع الحقول");
+    return;
+  }
+  Login() ;
+  form1.style.display = "none";
+  let user_board = document.querySelector(".container2");
+  user_board.style.display = "block";
+  closeOffensive()
+
+
+})
+// إجراء إنشاء حساب 
+createBtn.addEventListener("click", function (e) {
+  let form = e.target.closest("form"); // الفورم اللي الزرار موجود فيه
+  if (!inputValidation(form)) {
+    alert("⚠️ من فضلك املأ جميع الحقول");
+    return;
+  }
+  let user = getData() ; 
+if (emailExists(user.email))
+{
+  alert("هذا الإميل موجود بالفعل ! جرب إيميل أخر")
+  return
+}
+signIn() ;
+closeOffensive()
+});
+// دالة لإعداد بيانات البروفايل 
+function setProfileinfo(User) {
+  let profileName = document.querySelector(".user-avatar h3");
+  let profileEmail = document.querySelector(".user-email");
+  profileName.innerHTML = User.fname + " " + User.lname;
+  profileEmail.innerHTML = User.email;
+  setChar(User.fname) ;
+  document.querySelector("#staticBackdropLabel").style.display = "none" ;
+  document.querySelector(".note-text").style.display = "none" ;
+
+}
+//  دالة لإعداد ايقونة البروفايل 
+function setChar(Name){
+ let profileAvatar  ;
+ let logn_icon = document.querySelector("#login") ;
+ let sign_icon = document.querySelector("#sign") ;
+ let lognSignCont = document.querySelector(".lls") ;
+ profileAvatar = Name.charAt(0);
+ let circle = document.createElement("div") ; 
+ circle.setAttribute("data-bs-toggle", "offcanvas");
+ circle.setAttribute("data-bs-target", "#staticBackdrop");
+ circle.classList.add("show-profile") ;
+ circle.innerHTML = profileAvatar ; 
+ logn_icon.style.display = "none" ; 
+ sign_icon.style.display = "none" ; 
+lognSignCont.appendChild(circle) ;
+ 
+
+}
+function closeOffensive(){
+    let offcanvasElement = document.querySelector("#staticBackdrop"); 
+    let offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement); 
+    offcanvasInstance.hide(); // يقفل الـ offcanvas
+
+}
+
+
+//  localStorage.removeItem("users");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
